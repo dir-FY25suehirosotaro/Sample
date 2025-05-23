4659fd82e261dc5316649baa2ea61a32ff788355
dir-FY25suehirosotaro <sotaro.suehiro.w281@dirbato.co.jp>
2025-05-23 15:46:52 +0900

    Sample04

--- Sample/src/sample/Variable.java
+++ Sample/src/sample/Variable.java
@@ -4,11 +4,19 @@ public class Variable {
 
     public static void main(String[] args) {
         var nunber1 = 1;
+        //var:数値変数の定義
         System.out.println(nunber1);
 
        String str1;
        str1 = "abc";
+       //str:文字列変数の定義
        System.out.println(str1);
+       //println:改行付きprint
+       System.out.println();
+       
+       var secondsperday = 24*60*60;
+       System.out.println(secondsperday);
+       
     }
 
 }
