b6b683301215fc6dfcb912b81e3ad0acffb52fdb
dir-FY25suehirosotaro <sotaro.suehiro.w281@dirbato.co.jp>
2025-05-23 14:51:41 +0900

    sample03

--- Sample/src/sample/Test.java
+++ Sample/src/sample/Test.java
@@ -1,14 +1,21 @@
 package sample;
-
-import java.security.PublicKey;
+//所属パッケージの整理
 
 public class Test {
-
+//データ・それを操作するメソッドの整理
+//"public"：アクセス修辞子…「他のクラスからアクセス可能」
+//"{}"ブロック：区切りの範囲指定
     public static void main(String[] args) {
+        //メソッド：処理実行するソースコードのブロック
         System.out.println("Kiramex");
         System.out.println("Java");
         System.out.println("学習を始めましょう。");
-
+/*public:他のクラスからアクセス可能
+ *static:インスタンスを作成せずに呼び出し可能
+ *void:戻り値を返さない
+ *string[]arg:受け取り引数の指定
+ */
+//「●●を解決できません」：命令文のスペルミス
     }
 
 }
